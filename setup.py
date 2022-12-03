13fc1892e82ff902f67d56110114098798992878
elia-alonso-rueda <elia.alonsoru@alumnos.upm.es>
2022-12-03 16:22:28 +0100

    Edit setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='Homework 5 for Data Science Survival Skills',
       author='Elia Alonso',
       author_email='elia.alonso@fau.de',
       url='https://www.python.org/sigs/distutils-sig/',
-      packages=['numpy', 'turtles'],
+      install_requires=['numpy', 'turtles'],
      )
